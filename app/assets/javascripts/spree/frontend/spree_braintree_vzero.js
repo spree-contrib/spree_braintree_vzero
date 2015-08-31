bedf0b2a9d00f9c780e5ef53dacc095a13112049
Damian Legawiec <damian@sparksolutions.co>
2015-08-31 18:31:27 +0200

    [#100022924] simple payment gateway configuration and Drop-In UI rendering

--- app/assets/javascripts/spree/frontend/spree_braintree_vzero.js
+++ app/assets/javascripts/spree/frontend/spree_braintree_vzero.js
@@ -1,2 +1,31 @@
-// Placeholder manifest file.
-// the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+//= require spree/frontend
+
+SpreeBraintreeVzero = {
+  updateSaveAndContinueVisibility: function() {
+    if (this.isButtonHidden()) {
+      $(this).trigger('hideSaveAndContinue')
+    } else {
+      $(this).trigger('showSaveAndContinue')
+    }
+  },
+  isButtonHidden: function () {
+    paymentMethod = this.checkedPaymentMethod();
+    return (!$('#use_existing_card_yes:checked').length && SpreeBraintreeVzero.paymentMethodID && paymentMethod.val() == SpreeBraintreeVzero.paymentMethodID);
+  },
+  checkedPaymentMethod: function() {
+    return $('div[data-hook="checkout_payment_step"] input[type="radio"][name="order[payments_attributes][][payment_method_id]"]:checked');
+  },
+  hideSaveAndContinue: function() {
+    $("[data-hook=buttons]").hide();
+  },
+  showSaveAndContinue: function() {
+    $("[data-hook=buttons]").show();
+  }
+}
+
+$(document).ready(function() {
+  SpreeBraintreeVzero.updateSaveAndContinueVisibility();
+  paymentMethods = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
+    SpreeBraintreeVzero.updateSaveAndContinueVisibility();
+  });
+})
--- app/models/spree/gateway/braintree_vzero.rb
+++ app/models/spree/gateway/braintree_vzero.rb
@@ -1,8 +1,33 @@
+require 'braintree'
 module Spree
   class Gateway::BraintreeVzero < Gateway
+    preference :merchant_id, :string
+    preference :public_key, :string
+    preference :private_key, :string
+    preference :server, :string, default: :sandbox
+
+    def provider_class
+      Braintree
+    end
+
+    def provider
+      Braintree::Configuration.environment = preferred_server.present? ? preferred_server.to_sym : :sandbox
+      Braintree::Configuration.merchant_id = preferred_merchant_id
+      Braintree::Configuration.public_key = preferred_public_key
+      Braintree::Configuration.private_key = preferred_private_key
+      Braintree
+    end
+
+    def auto_capture?
+      true
+    end
 
     def method_type
       'braintree_vzero'
+    end
+
+    def client_token
+      provider::ClientToken.generate
     end
 
   end
