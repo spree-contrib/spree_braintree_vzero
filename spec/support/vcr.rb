73f1bf3a5e8b6c49c7ed959f941c4482c53ec4ac
Dominik Halat <dominik.halat@sparksolutions.co>
2015-09-03 01:58:19 +0200

    [#100022924] - Change VCR request matching strategy

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,5 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
-  config.default_cassette_options = {match_requests_on: [:method, :path], record: :new_episodes}
+  config.default_cassette_options = {match_requests_on: [:method, :host], record: :new_episodes}
 end
