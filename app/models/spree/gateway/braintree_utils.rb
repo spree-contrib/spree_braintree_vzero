2d8fccefa425de12f75c1c29760f2584a3bcdcb4
Damian Legawiec <damian@sparksolutions.co>
2015-09-15 13:04:03 +0200

    duplicated key warnings removed

--- app/models/spree/gateway/braintree_utils.rb
+++ app/models/spree/gateway/braintree_utils.rb
@@ -9,27 +9,27 @@ module Spree
       end
 
       def order_data(nonce)
-        {amount: order.total,
-         payment_method_nonce: nonce,
-         amount: order.total,
-         payment_method_nonce: nonce,
-         order_id: order.number}
+        {
+          amount: order.total,
+          payment_method_nonce: nonce,
+          order_id: order.number
+        }
       end
 
       def address_data(address_type)
         {
-            company: order.send("#{address_type}_address").company,
-            country_code_alpha2: order.send("#{address_type}_address").country.iso,
-            country_code_alpha3: order.send("#{address_type}_address").country.iso3,
-            country_code_numeric: order.send("#{address_type}_address").country.numcode,
-            country_name: order.send("#{address_type}_address").country.name,
-            first_name: order.send("#{address_type}_address").first_name,
-            last_name: order.send("#{address_type}_address").last_name,
-            locality: order.send("#{address_type}_address").city,
-            postal_code: order.send("#{address_type}_address").zipcode,
-            region: order.send("#{address_type}_address").state.try(:abbr),
-            street_address: order.send("#{address_type}_address").address1,
-            extended_address: order.send("#{address_type}_address").address2,
+          company: order.send("#{address_type}_address").company,
+          country_code_alpha2: order.send("#{address_type}_address").country.iso,
+          country_code_alpha3: order.send("#{address_type}_address").country.iso3,
+          country_code_numeric: order.send("#{address_type}_address").country.numcode,
+          country_name: order.send("#{address_type}_address").country.name,
+          first_name: order.send("#{address_type}_address").first_name,
+          last_name: order.send("#{address_type}_address").last_name,
+          locality: order.send("#{address_type}_address").city,
+          postal_code: order.send("#{address_type}_address").zipcode,
+          region: order.send("#{address_type}_address").state.try(:abbr),
+          street_address: order.send("#{address_type}_address").address1,
+          extended_address: order.send("#{address_type}_address").address2,
         }
       end
 
