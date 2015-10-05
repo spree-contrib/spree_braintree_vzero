30dd5c74cbaf6a50453cb292d3a86f72c9cad2b5
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-05 15:07:26 +0200

    [#100101122] Add preferences and partials for DropinUI

--- app/models/spree/gateway/braintree_vzero.rb
+++ app/models/spree/gateway/braintree_vzero.rb
@@ -10,6 +10,9 @@ module Spree
     preference :advanced_fraud_tools, :boolean_select, default: false
     preference :store_payments_in_vault, :select, default: -> { {values: [:do_not_store, :store_only_on_success, :store_all]} }
     preference :descriptor_name, :string
+    preference :dropin_container, :string, default: 'payment-form'
+    preference :dropin_checkout_form_id, :string, default: 'checkout_form_payment'
+    preference :dropin_error_messages_container_id, :string, default: 'content'
 
     attr_reader :utils
 
