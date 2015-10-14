b3019d2b1dfb704d9f102216860ccfca0b4d6ed3
Dominik Halat <dominik.halat@sparksolutions.co>
2015-10-14 23:42:57 +0200

    [finishes #100308856] - Basic Hosted Fields with configurable selectors and placeholders

--- app/models/spree/gateway/braintree_vzero_standard.rb
+++ app/models/spree/gateway/braintree_vzero_standard.rb
@@ -4,6 +4,14 @@ module Spree
     preference :dropin_container, :string, default: 'payment-form'
     preference :dropin_checkout_form_id, :string, default: 'checkout_form_payment'
     preference :dropin_error_messages_container_id, :string, default: 'content'
+    preference :hosted_container, :string, default: 'payment-form'
+    preference :hosted_number_selector, :string, default: '#hosted-fields-number'
+    preference :hosted_number_placeholder, :string, default: I18n.t(:number, scope: 'braintree.preferences.placeholder')
+    preference :hosted_cvv_selector, :string, default: '#hosted-fields-cvv'
+    preference :hosted_cvv_placeholder, :string, default: I18n.t(:cvv, scope: 'braintree.preferences.placeholder')
+    preference :hosted_expiration_date_selector, :string, default: '#hosted-fields-expiration-date'
+    preference :hosted_expiration_date_placeholder, :string, default: I18n.t(:expiration_date, scope: 'braintree.preferences.placeholder')
+
 
 
     def reject_preferences
