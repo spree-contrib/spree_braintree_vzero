f3bc4de0f991da26898d1a5bfe570029c7e9846c
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-20 09:55:05 +0200

    [#103411118] Add checkout confirmation step for BraintreeVzeroPaypalExpress payment

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,12 @@
 Spree::Order.class_eval do
+  checkout_flow do
+    go_to_state :address
+    go_to_state :delivery
+    go_to_state :payment, if: ->(order) { order.payment_required? }
+    go_to_state :confirm, if: ->(order) { order.confirmation_required? }
+    go_to_state :complete
+  end
+
   state_machine.before_transition to: :complete, do: :process_paypal_express_payments
 
   def save_paypal_address(type, address_hash)
