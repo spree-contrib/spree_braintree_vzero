cebb3e0271641f81bda6efc0640d77a2e023600c
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-20 17:09:24 +0200

    [#105774688] Rename gateway BraintreeVzeroStandard to BraintreeVzeroDropInUI

--- lib/spree_braintree_vzero/engine.rb
+++ lib/spree_braintree_vzero/engine.rb
@@ -18,7 +18,7 @@ module SpreeBraintreeVzero
     config.to_prepare &method(:activate).to_proc
 
     initializer "spree.braintree_vzero.payment_methods", :after => "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::Gateway::BraintreeVzeroStandard
+      app.config.spree.payment_methods << Spree::Gateway::BraintreeVzeroDropInUI
       app.config.spree.payment_methods << Spree::Gateway::BraintreeVzeroPaypalExpress
     end
   end
