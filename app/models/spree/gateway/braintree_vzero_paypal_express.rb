b0f2afc0efabf73c7983c336f5883a135e678c5d
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-22 12:35:30 +0200

    [#103491768] Single use ad store_payment_in_vault

--- app/models/spree/gateway/braintree_vzero_paypal_express.rb
+++ app/models/spree/gateway/braintree_vzero_paypal_express.rb
@@ -1,7 +1,7 @@
 module Spree
   class Gateway::BraintreeVzeroPaypalExpress < Spree::Gateway::BraintreeVzeroBase
     preference :paypal_display_on_cart, :boolean_select, default: true
-    preference :single_use, :boolean_select, default: true
+    preference :store_payments_in_vault, :select, default: -> { { values: [:do_not_store, :store_all] } }
 
     def push_order_to_delivery(order, email)
       order.update_column(:email, email)
