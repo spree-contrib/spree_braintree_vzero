da357f41c14d2c223dd8223793cbd571471d63aa
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-23 13:38:06 +0200

    [#103409918] Add email completion

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -7,4 +7,12 @@ Spree::CheckoutController.class_eval do
     response.headers['X-Frame-Options'] = 'ALLOW-FROM https://assets.braintreegateway.com'
   end
 
+  def check_registration
+    return unless Spree::Auth::Config[:registration_step]
+    paypal_express = current_order.payments.valid.map(&:payment_method).compact.any? { |p| p.kind_of?(Spree::Gateway::BraintreeVzeroPaypalExpress) }
+    return if spree_current_user || (!paypal_express && current_order.email)
+    store_location
+    redirect_to spree.checkout_registration_path
+  end
+
 end
--- app/models/spree/gateway/braintree_vzero_paypal_express.rb
+++ app/models/spree/gateway/braintree_vzero_paypal_express.rb
@@ -6,7 +6,6 @@ module Spree
     def push_order_to_delivery(order, email)
       order.update_column(:email, email)
       order.next! until order.state.eql?('delivery')
-      order.update_column(:email, nil)
     end
   end
 end
