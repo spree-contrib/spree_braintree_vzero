6ef44330e63b4fe9f38bb05767cb09c4c7f126c5
Mafi <michal.markiewicz@sparksolutions.co>
2015-10-27 10:28:04 +0100

    [#104175200] Add display name preference for paypal express gateway

--- app/models/spree/gateway/braintree_vzero_paypal_express.rb
+++ app/models/spree/gateway/braintree_vzero_paypal_express.rb
@@ -2,6 +2,7 @@ module Spree
   class Gateway::BraintreeVzeroPaypalExpress < Spree::Gateway::BraintreeVzeroBase
     preference :paypal_display_on_cart, :boolean_select, default: true
     preference :store_payments_in_vault, :select, default: -> { { values: [:do_not_store, :store_all] } }
+    preference :paypal_display_name, :string
 
     def push_order_to_state(order, state, email)
       order.update_column(:email, email)
