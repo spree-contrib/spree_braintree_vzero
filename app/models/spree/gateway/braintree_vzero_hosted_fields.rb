3e4a5ea4c37db94e31aba4486a923474ffbfef77
Mafi <michal.markiewicz@sparksolutions.co>
2015-11-12 14:35:27 +0100

    [#100308856] Fix hosted fields placeholders

--- app/models/spree/gateway/braintree_vzero_hosted_fields.rb
+++ app/models/spree/gateway/braintree_vzero_hosted_fields.rb
@@ -4,11 +4,11 @@ module Spree
     preference :error_messages_container_id, :string, default: 'content'
     preference :hosted_fields_container, :string, default: 'payment-form'
     preference :hosted_fields_number_selector, :string, default: '#hosted-fields-number'
-    preference :hosted_fields_number_placeholder, :string, default: I18n.t(:number, scope: 'braintree.preferences.placeholder')
+    preference :hosted_fields_number_placeholder, :string, default: 'Card Number'
     preference :hosted_fields_cvv_selector, :string, default: '#hosted-fields-cvv'
-    preference :hosted_fields_cvv_placeholder, :string, default: I18n.t(:cvv, scope: 'braintree.preferences.placeholder')
+    preference :hosted_fields_cvv_placeholder, :string, default: 'Cvv code'
     preference :hosted_fields_expiration_date_selector, :string, default: '#hosted-fields-expiration-date'
-    preference :hosted_fields_expiration_date_placeholder, :string, default: I18n.t(:expiration_date, scope: 'braintree.preferences.placeholder')
+    preference :hosted_fields_expiration_date_placeholder, :string, default: 'Card Expiration Date'
     preference :store_payments_in_vault, :select, default: -> { { values: [:do_not_store, :store_only_on_success, :store_all] } }
     preference :'3dsecure', :boolean_select, default: false
   end
