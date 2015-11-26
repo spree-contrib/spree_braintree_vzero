9a1e4b5671b81723de41268ae3a7d8e818ddd706
Mafi <michal.markiewicz@sparksolutions.co>
2015-11-26 17:18:26 +0100

    [#108939002] Add paypal express payment method on cart

--- app/overrides/spree/checkout/_payment.rb
+++ app/overrides/spree/checkout/_payment.rb
@@ -1,6 +1,12 @@
 Deface::Override.new(
   virtual_path: 'spree/checkout/_payment',
   name: 'Removes Paypal Express payment method from checkout',
-  insert_after: 'erb[silent]:contains("available_payment_methods")',
-  text: '<% next if method.is_a?(Spree::Gateway::BraintreeVzeroPaypalExpress) %>'
+  surround: 'erb[loud]:contains("Spree.t(method.name, :scope => :payment_methods, :default => method.name)")',
+  text: %Q{
+    <% if method.is_a?(Spree::Gateway::BraintreeVzeroPaypalExpress) %>
+      <a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup', 'WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"><img src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" border="0" alt="PayPal Logo"> What is PayPal?</a>
+    <% else %>
+      <%= render_original %>
+    <% end %>
+  }
 )
