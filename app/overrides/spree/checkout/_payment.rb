1d0bf8a209d580226499d8bdf659d8674177a172
Mafi <michal.markiewicz@sparksolutions.co>
2015-12-02 09:25:26 +0100

    [#108939002] Remove link from PayPal logo

--- app/overrides/spree/checkout/_payment.rb
+++ app/overrides/spree/checkout/_payment.rb
@@ -4,7 +4,8 @@ Deface::Override.new(
   surround: 'erb[loud]:contains("Spree.t(method.name, :scope => :payment_methods, :default => method.name)")',
   text: %Q{
     <% if method.is_a?(Spree::Gateway::BraintreeVzeroPaypalExpress) %>
-      <a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup', 'WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"><img src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" border="0" alt="PayPal Logo"> What is PayPal?</a>
+      <img src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" border="0" alt="PayPal Logo">
+      <a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup', 'WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"> What is PayPal?</a>
     <% else %>
       <%= render_original %>
     <% end %>
