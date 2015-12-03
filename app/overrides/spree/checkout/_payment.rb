7fc991ce0902cf045cf0ac9668dc5c31619aa5a8
Mafi <michal.markiewicz@sparksolutions.co>
2015-12-03 16:15:26 +0100

    [#108939002] Use translations for paypal text and title

--- app/overrides/spree/checkout/_payment.rb
+++ app/overrides/spree/checkout/_payment.rb
@@ -5,7 +5,7 @@ Deface::Override.new(
   text: %Q{
     <% if method.is_a?(Spree::Gateway::BraintreeVzeroPaypalExpress) %>
       <img src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" border="0" alt="PayPal Logo">
-      <a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="How PayPal Works" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup', 'WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"> What is PayPal?</a>
+      <a href="https://www.paypal.com/webapps/mpp/paypal-popup" title="<%= Spree.t(:how_paypal_works) %>" onclick="javascript:window.open('https://www.paypal.com/webapps/mpp/paypal-popup', 'WIPaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1060, height=700'); return false;"> <%= Spree.t(:what_is_paypal) %></a>
     <% else %>
       <%= render_original %>
     <% end %>
