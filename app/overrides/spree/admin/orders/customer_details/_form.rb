5847c3419394234c585971056f07905e51eb3e9a
Mafi <michal.markiewicz@sparksolutions.co>
2016-03-03 13:45:40 +0100

    Add warning for braintree admins

---
+++ app/overrides/spree/admin/orders/customer_details/_form.rb
@@ -0,0 +1,8 @@
+Deface::Override.new(
+  virtual_path: 'spree/admin/orders/customer_details/_form',
+  name: 'Add warning for braintree admins',
+  insert_after: '[data-hook="customer_fields"]',
+  text: '<div class="alert alert-warning">
+           <%= I18n.t("braintree.admin.guest_checkout_warning") %>
+         </div>'
+)
