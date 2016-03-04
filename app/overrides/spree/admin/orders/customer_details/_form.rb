598b6ab50b19567342bbd12e2b980c47c229ee03
Mafi <michal.markiewicz@sparksolutions.co>
2016-03-04 14:58:38 +0100

    After merge fixes

--- app/overrides/spree/admin/orders/customer_details/_form.rb
+++ app/overrides/spree/admin/orders/customer_details/_form.rb
@@ -2,7 +2,7 @@ Deface::Override.new(
   virtual_path: 'spree/admin/orders/customer_details/_form',
   name: 'Add warning for braintree admins',
   insert_after: '[data-hook="customer_fields"]',
-  text: '<div class="alert alert-warning">
+  text: '<div class="promotion-block">
            <%= I18n.t("braintree.admin.guest_checkout_warning") %>
          </div>'
 )
