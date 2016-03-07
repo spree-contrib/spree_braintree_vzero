0a87c35534badea60108769abcd37b3aa7412cc0
Alessandro Lepore <lepore.ale@gmail.com>
2016-03-07 18:07:54 +0100

    more preference tweaks

--- app/models/spree/gateway/braintree_vzero_drop_in_ui.rb
+++ app/models/spree/gateway/braintree_vzero_drop_in_ui.rb
@@ -3,7 +3,7 @@ module Spree
     preference :dropin_container, :string, default: 'payment-form'
     preference :checkout_form_id, :string, default: 'checkout_form_payment'
     preference :error_messages_container_id, :string, default: 'content'
-    preference :store_payments_in_vault, :select, default: -> { { values: [:do_not_store, :store_only_on_success, :store_all] } }
+    preference :store_payments_in_vault, :select, default: { values: [:do_not_store, :store_only_on_success, :store_all] }
     preference :'3dsecure', :boolean_select, default: false
 
     after_save :disable_hosted_gateways, if: proc { active? && (active_changed? || id_changed?) }
--- app/models/spree/gateway/braintree_vzero_paypal_express.rb
+++ app/models/spree/gateway/braintree_vzero_paypal_express.rb
@@ -1,7 +1,7 @@
 module Spree
   class Gateway::BraintreeVzeroPaypalExpress < Spree::Gateway::BraintreeVzeroBase
     preference :paypal_display_on_cart, :boolean_select, default: true
-    preference :store_payments_in_vault, :select, default: -> { { values: [:do_not_store, :store_all] } }
+    preference :store_payments_in_vault, :select, default: { values: [:do_not_store, :store_all] }
     preference :paypal_display_name, :string
     preference :checkout_form_id, :string, default: 'checkout_form_payment'
     preference :error_messages_container_id, :string, default: 'content'
