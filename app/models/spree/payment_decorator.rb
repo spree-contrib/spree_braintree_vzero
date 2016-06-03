0668d3739027e7b68596ae7ae2881375c88cf0ac
Mafi <michal.markiewicz@sparksolutions.co>
2016-06-03 11:57:47 +0200

    Use alias instead of overriding methods

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,19 +1,9 @@
 Spree::Payment.class_eval do
+  alias_method :original_update_order, :update_order
+
   def update_order
     # without reload order was updated with inaccurate data
     order.reload
-    if completed? || void?
-      order.updater.update_payment_total
-    end
-
-    if order.completed?
-      order.updater.update_payment_state
-      order.updater.update_shipments
-      order.updater.update_shipment_state
-    end
-
-    if self.completed? || order.completed?
-      order.persist_totals
-    end
+    original_update_order
   end
 end
