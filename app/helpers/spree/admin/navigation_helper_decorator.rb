d5e7ebaa2670857aa20d718f52d16e2903a08953
Mafi <michal.markiewicz@sparksolutions.co>
2016-06-03 11:57:47 +0200

    Use alias instead of overriding methods

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -1,15 +1,8 @@
 Spree::Admin::NavigationHelper.module_eval do
+  alias_method :original_link_to_with_icon, :link_to_with_icon
+
   def link_to_with_icon(icon_name, text, url, options = {})
     icon_name = 'capture' if icon_name.eql?('settle')
-    options[:class] = (options[:class].to_s + " icon-link with-tip action-#{icon_name}").strip
-    options[:class] += ' no-text' if options[:no_text]
-    options[:title] = text if options[:no_text]
-    text = options[:no_text] ? '' : content_tag(:span, text, class: 'text')
-    options.delete(:no_text)
-    if icon_name
-      icon = content_tag(:span, '', class: "icon icon-#{icon_name}")
-      text.insert(0, icon + ' ')
-    end
-    link_to(text.html_safe, url, options)
+    original_link_to_with_icon(icon_name, text, url, options)
   end
 end
