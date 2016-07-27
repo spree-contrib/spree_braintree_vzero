9411633d47773dd596cf63a1bf3d0fa9ade77574
Damian Legawiec <damian@sparksolutions.co>
2016-07-27 11:30:23 +0200

    We should only decorate Spree::UserSessionsController if it's defined
    Not every spree project uses `spree_auth_devise`

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,7 +1,9 @@
-Spree::UserSessionsController.class_eval do
-  before_action :associate_user, only: :create
+if defined?(Spree::UserSessionsController)
+  Spree::UserSessionsController.class_eval do
+    before_action :associate_user, only: :create
 
-  def current_order_params
-    { currency: current_currency, guest_token: cookies.signed[:guest_token], store_id: current_store.id }
+    def current_order_params
+      { currency: current_currency, guest_token: cookies.signed[:guest_token], store_id: current_store.id }
+    end
   end
 end
