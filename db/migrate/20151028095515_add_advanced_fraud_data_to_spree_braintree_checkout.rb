82546f4140e99f9bcc639d58d1cfec44769ab18c
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2016-09-07 21:44:03 +0200

    add 4.2 version to migrations

--- db/migrate/20151028095515_add_advanced_fraud_data_to_spree_braintree_checkout.rb
+++ db/migrate/20151028095515_add_advanced_fraud_data_to_spree_braintree_checkout.rb
@@ -1,4 +1,4 @@
-class AddAdvancedFraudDataToSpreeBraintreeCheckout < ActiveRecord::Migration
+class AddAdvancedFraudDataToSpreeBraintreeCheckout < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_braintree_checkouts, :advanced_fraud_data, :string
   end
