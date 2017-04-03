168a67349b252c4aee40430f5f491c7b666d14e1
Dmytro Koval <dmytro.koval@sparksolutions.co>
2017-04-03 10:50:13 +0200

    Fix polymorphic association for some join cases

--- app/models/spree/braintree_checkout.rb
+++ app/models/spree/braintree_checkout.rb
@@ -7,7 +7,7 @@ module Spree
 
     FINAL_STATES = %w(authorization_expired processor_declined gateway_rejected failed voided settled settlement_declined refunded released).freeze
 
-    has_one :payment, foreign_key: :source_id, as: :source
+    has_one :payment, foreign_key: :source_id, as: :source, class_name: 'Spree::Payment'
     has_one :order, through: :payment
 
     def self.create_from_params(params)
