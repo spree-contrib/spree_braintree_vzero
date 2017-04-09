086c36cfdb0aa37ba89d22974f8c43d68ad14cab
Mafi <mmafi88@gmail.com>
2017-04-09 13:14:27 +0200

    Allow braintree successful_response to receive message

--- spec/factories/payment_factory_decorator.rb
+++ spec/factories/payment_factory_decorator.rb
@@ -16,4 +16,13 @@ FactoryGirl.define do
     state 'checkout'
     braintree_nonce 'fake-valid-nonce'
   end
+
+  factory :braintree_vzero_failed_paypal_payment, class: Spree::Payment do
+    amount 0
+    association(:payment_method, factory: :vzero_paypal_gateway)
+    association(:source, factory: :braintree_checkout)
+    order
+    state 'checkout'
+    braintree_nonce 'fake-invalid-nonce'
+  end
 end
