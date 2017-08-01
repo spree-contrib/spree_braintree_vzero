44a4576e839f62ec8128562e529aa4e485b335cf
Damian Legawiec <damian@sparksolutions.co>
2017-08-01 12:58:51 +0200

    Fixed controller specs for Rails 5.1

--- spec/controllers/spree/api/v1/braintree_client_token_controller_spec.rb
+++ spec/controllers/spree/api/v1/braintree_client_token_controller_spec.rb
@@ -7,14 +7,14 @@ describe Spree::Api::V1::BraintreeClientTokenController, :vcr, type: :controller
   describe 'POST #create' do
     context 'guest checkout' do
       it 'returns proper json data when gateway not specified' do
-        post :create, order_number: order.number
+        post :create, params: { order_number: order.number }
         expect(response).to have_http_status 200
         expect(json_response['client_token']).not_to be nil
         expect(json_response['payment_method_id']).not_to be nil
       end
 
       it 'returns proper json data when gateway specified' do
-        post :create, payment_method_id: gateway.id, order_number: order.number
+        post :create, params: { payment_method_id: gateway.id, order_number: order.number }
         expect(response).to have_http_status 200
         expect(json_response['client_token']).not_to be nil
         expect(json_response['payment_method_id']).to eq gateway.id
@@ -26,14 +26,14 @@ describe Spree::Api::V1::BraintreeClientTokenController, :vcr, type: :controller
       before { user.generate_spree_api_key! }
 
       it 'returns proper json data when gateway not specified' do
-        post :create, order_number: order.number, token: user.spree_api_key
+        post :create, params: { order_number: order.number, token: user.spree_api_key }
         expect(response).to have_http_status 200
         expect(json_response['client_token']).not_to be nil
         expect(json_response['payment_method_id']).not_to be nil
       end
 
       it 'returns proper json data when gateway specified' do
-        post :create, payment_method_id: gateway.id, token: user.spree_api_key, order_number: order.number
+        post :create, params: {  payment_method_id: gateway.id, token: user.spree_api_key, order_number: order.number }
         expect(response).to have_http_status 200
         expect(json_response['client_token']).not_to be nil
         expect(json_response['payment_method_id']).to eq gateway.id
