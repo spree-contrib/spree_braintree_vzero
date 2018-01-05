791c37bcef7e331d568d1bf373d00c418debf4df
JuanFrancisco Crespo <JF_Crespo@outlook.es>
2018-01-05 17:41:45 +0100

    Some rubocop fixes

--- app/controllers/spree/admin/payments_controller_decorator.rb
+++ app/controllers/spree/admin/payments_controller_decorator.rb
@@ -4,13 +4,17 @@ Spree::Admin::PaymentsController.class_eval do
   private
   
   def init_braintree
-    unless @payment_method.store_credit?
-      @payment ||= @order.payments.build(object_params)
-      if @payment.payment_method.source_required? && @payment.payment_source.is_a?(Spree::Gateway::BraintreeVzeroBase)
-        @payment.braintree_token = params[:payment_method_token]
-        @payment.braintree_nonce = params[:payment_method_nonce]
-        @payment.source = Spree::BraintreeCheckout.create!(admin_payment: true)
-      end
+    return if @payment_method.store_credit?
+    @payment ||= @order.payments.build(object_params)
+    if braintree_source?(@payment)
+      @payment.braintree_token = params[:payment_method_token]
+      @payment.braintree_nonce = params[:payment_method_nonce]
+      @payment.source = Spree::BraintreeCheckout.create!(admin_payment: true)
     end
+  end
+
+  def braintree_source?(payment)
+    return payment.payment_method.source_required? && 
+          payment.payment_source.is_a?(Spree::Gateway::BraintreeVzeroBase)
   end
 end
