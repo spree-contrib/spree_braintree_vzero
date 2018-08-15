f3b06af912c87c55415ca73a385aaf6df7751a43
Damian Legawiec <damian@sparksolutions.co>
2018-08-15 17:15:07 +0200

    Bump version to 3.4.0

--- lib/spree_braintree_vzero/version.rb
+++ lib/spree_braintree_vzero/version.rb
@@ -9,8 +9,8 @@ module SpreeBraintreeVzero
 
   module VERSION
     MAJOR = 3
-    MINOR = 3
-    TINY  = 3
+    MINOR = 4
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
