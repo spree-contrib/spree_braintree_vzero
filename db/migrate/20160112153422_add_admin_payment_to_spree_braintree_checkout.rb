02935462928c489d0e7904c472787d80485e569c
Przemek Kosakowski <p.kosakowski@gmail.com>
2020-11-12 09:13:10 +0100

    Change type name

--- db/migrate/20160112153422_add_admin_payment_to_spree_braintree_checkout.rb
+++ db/migrate/20160112153422_add_admin_payment_to_spree_braintree_checkout.rb
@@ -1,5 +1,5 @@
 class AddAdminPaymentToSpreeBraintreeCheckout < SpreeExtension::Migration[4.2]
   def change
-    add_column :spree_braintree_checkouts, :admin_payment, :bool
+    add_column :spree_braintree_checkouts, :admin_payment, :boolean
   end
 end
