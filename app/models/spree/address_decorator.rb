ff22989b944602b2a33d49f1f538934854ceca18
Kacper Mękarski <43354669+KacperMekarski@users.noreply.github.com>
2021-09-30 15:09:04 +0200

    Fix vaulted duplicates method for older ruby versions (#266)
    * fix Spree::Address.vaulted_duplicates method for older ruby versions
    
    * add newline

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   module AddressDecorator
     def self.prepended(base)
       base.scope :vaulted_duplicates, ->(address) do
-        where.not(id: address.id).
+        base.where.not(id: address.id).
           where.not(braintree_id: nil).
           where(address.attributes.except('id', 'updated_at', 'created_at', 'braintree_id', 'preferences'))
       end
