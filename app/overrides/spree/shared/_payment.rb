2222966526321b0392abca969b21600c8f34142d
Michael Davidson <mdavo6@gmail.com>
2022-05-24 14:12:30 +1000

    Remove logos from payment partial

--- app/overrides/spree/shared/_payment.rb
+++ app/overrides/spree/shared/_payment.rb
@@ -5,24 +5,14 @@ Deface::Override.new(
   text: %{
         <% elsif payment.payment_method.kind_of?(Spree::Gateway::BraintreeVzeroBase) %>
           <% if (last_digits = payment.source.braintree_last_digits) %>
-            <%
-              cc_type = payment.source.braintree_card_type
-              img = "credit_cards/icons/" + cc_type.downcase + ".png"
-            %>
-            <% if asset_available?(img) %>
-              <%= image_tag img %>
-            <% else %>
-              <p><%= Spree.t(:cc_type) + ": " + cc_type %></p>
-              </br>
-            <% end %>
+            <% cc_type = payment.source.braintree_card_type %>
+            <p><%= cc_type %></p>
             <p><%= Spree.t(:ending_in) + " " + last_digits %></p>
           <% end %>
-
           <% if (paypal_email = payment.source.paypal_email) %>
-            <!-- PayPal Logo --><img src="https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg" border="0" alt="PayPal Logo"><!-- PayPal Logo -->
-            <%= paypal_email %>
+            <p>Paypal</p>
+            <p><%= paypal_email %></p>
           <% end %>
-
         <% else %>
         }
 )
